fc9dbe3215a0996aca7d2cdaea8e5df76d1a3c84
asmi2 <asmi24886@gmail.com>
2025-03-14 16:15:18 +0530

    knapsack optimized solution added

--- src/main/java/com/solutions/adityaverma/dp/Knapsack01.java
+++ src/main/java/com/solutions/adityaverma/dp/Knapsack01.java
@@ -1,7 +1,10 @@
 package com.solutions.adityaverma.dp;
 
 
-//Maximum profits
+import java.util.Arrays;
+import java.util.stream.IntStream;//Maximum profits
+import java.util.stream.Stream;
+
 public class Knapsack01 {
 
     static class Solution {
@@ -35,11 +38,60 @@ public class Knapsack01 {
 
             return dp[dp_i - 1][dp_j - 1];
         }
+
+        public int max_profit_2_arrays() {
+            int dp_i = profits.length + 1;
+            int dp_j = W + 1;
+            int [] dp_previous = IntStream.generate(() -> 0).limit(dp_j).toArray();
+            int [] dp_current = IntStream.generate(() -> 0).limit(dp_j).toArray();
+
+            for(int i=1; i<dp_i; i++) {
+                for(int j=0; j<dp_j; j++) {
+
+                    int include_not = dp_previous[j];
+                    if(weights[i-1] > j) {
+                        dp_current[j] = include_not;
+                        continue;
+                    }
+
+                    int include = profits[i-1] + dp_previous[j - weights[i-1]];
+                    dp_current[j] = Math.max(include, include_not);
+                }
+
+                // Just swap references, no need to reset values
+                int[] temp = dp_previous;
+                dp_previous = dp_current;
+                dp_current = temp;
+            }
+
+            return dp_previous[dp_j - 1];
+        }
+
+        public int max_profit_optimized() {
+            int dp_i = profits.length + 1;
+            int dp_j = W + 1;
+
+            int [] dp_current = IntStream.generate(() -> 0).limit(dp_j).toArray();
+
+            for(int i=1; i<dp_i; i++) {
+                for(int j=dp_j - 1; j>=weights[i - 1]; j--) {
+
+                    int include = profits[i-1] + dp_current[j - weights[i-1]];
+                    if(include > dp_current[j])
+                        dp_current[j] = include;
+                }
+            }
+
+            return dp_current[dp_j - 1];
+        }
     }
+
 
     public static void main(String [] args) {
         Solution solution = new Solution();
         System.out.println(solution.max_profit_2d());
+        System.out.println(solution.max_profit_2_arrays());
+        System.out.println(solution.max_profit_optimized());
 
     }
 }
