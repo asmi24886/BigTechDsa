1213688b1d934d274ac1f3df65f3582db91c78a5
asmi2 <asmi24886@gmail.com>
2025-03-18 23:40:53 +0530

    non optimized combination sum ii added

---
+++ src/main/java/com/solutions/neetcode/backtracking/LC40/CombinationSum2.java
@@ -0,0 +1,48 @@
+package com.solutions.neetcode.backtracking.LC40;
+
+import java.util.*;
+//TODO optimize
+public class CombinationSum2 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int [] candidates = {10,1,2,7,6,1,5};
+        sol.combinationSum2(candidates, 8).forEach(
+                System.out::println
+        );
+    }
+
+
+
+    static class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(candidates);
+            backtrack(candidates, target, result, new ArrayList<>(), 0, 0);
+            return result.stream().toList();
+        }
+
+        public void backtrack(int [] candidates, int target, List<List<Integer>> result,
+                             ArrayList<Integer> solution, int currentSum, int index) {
+
+            if(currentSum == target) {
+                result.add(new ArrayList<>(solution));
+                return;
+            }
+
+            if(index >= candidates.length) {
+                return;
+            }
+
+            solution.add(candidates[index]);
+            backtrack(candidates, target, result, solution, currentSum + candidates[index], index + 1);
+            solution.remove(solution.size() - 1);
+
+            while(index + 1 < candidates.length && candidates[index] == candidates[index + 1] ) index++;
+            backtrack(candidates, target, result, solution, currentSum, index + 1);
+        }
+    }
+
+
+
+}
