ec8dd8390bb36bcfdb69bd43796eb6c4984bda31
asmi2 <asmi24886@gmail.com>
2025-07-20 17:09:07 +0530

    ok

---
+++ src/main/java/com/solutions/neetcode/heap/LC502/IPO.java
@@ -0,0 +1,34 @@
+package com.solutions.neetcode.heap.LC502;
+
+import java.util.*;
+
+public class IPO {
+    public static void main(String[] args) {
+        new Solution().findMaximizedCapital(2, 0, new int[]{1,2,3}, new int[]{0,1,1});
+    }
+
+    static class Solution {
+        public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+
+            PriorityQueue<int []> capitalToProfitMinHeap = new PriorityQueue<>(Comparator.comparingInt(i -> i[1]));
+            PriorityQueue<Integer> profitMaxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+            for(int i = 0; i < profits.length; i++) {
+                capitalToProfitMinHeap.add(new int[]{profits[i], capital[i]});
+            }
+
+            while(k!=0) {
+
+                while(!capitalToProfitMinHeap.isEmpty() && w >= capitalToProfitMinHeap.peek()[1]) {
+                    profitMaxHeap.add(capitalToProfitMinHeap.remove()[0]);
+                }
+
+                if(profitMaxHeap.isEmpty()) return w;
+
+                w+=profitMaxHeap.remove();
+                k--;
+            }
+            return w;
+        }
+    }
+}
