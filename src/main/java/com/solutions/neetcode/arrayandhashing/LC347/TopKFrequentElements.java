3c67af56826a1a39de0ea369dc3a025f79e4c15d
asmi2 <asmi24886@gmail.com>
2025-08-19 12:08:58 +0530

    ok

--- src/main/java/com/solutions/neetcode/arrayandhashing/LC347/TopKFrequentElements.java
+++ src/main/java/com/solutions/neetcode/arrayandhashing/LC347/TopKFrequentElements.java
@@ -32,5 +32,52 @@ public class TopKFrequentElements {
 
             return ans;
         }
+
+        public int[] topKFrequent_bucketSort(int[] nums, int k) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for(int i :  nums) {
+                map.putIfAbsent(i, 0);
+                map.put(i, map.get(i) + 1);
+            }
+
+            ArrayList<Integer> [] countBuckets = new ArrayList[nums.length + 1];
+
+            Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+            for(Map.Entry<Integer, Integer> e : entries) {
+                if(countBuckets[e.getValue()] == null) {
+                    countBuckets[e.getValue()] = new ArrayList<>();
+                }
+
+                countBuckets[e.getValue()].add(e.getKey());
+            }
+
+            int i = nums.length;
+            int [] result = new int[k];
+            while(true) {
+                if(k == 0 || i == 0) {
+                    break;
+                }
+
+                if(countBuckets[i] == null) {
+                    i--;
+                    continue;
+                }
+
+                List<Integer> keys = countBuckets[i--];
+
+                for(int key : keys) {
+                    k--;
+                    result[k] = key;
+
+                    if(k == 0) {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
     }
+
 }
