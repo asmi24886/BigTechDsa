44edf58d3c97d33c0ed7f4d48150c2a3cf7284c0
asmi2 <asmi24886@gmail.com>
2025-08-19 22:05:40 +0530

    ok

---
+++ src/main/java/com/solutions/neetcode/arrayandhashing/LC238/ProductOfArrayExceptSelf.java
@@ -0,0 +1,50 @@
+package com.solutions.neetcode.arrayandhashing.LC238;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String [] s) {
+        Arrays.stream(new Solution().productExceptSelf(new int[]{-1,1,0,-3,3})).forEach(System.out::println);
+    }
+    static class Solution {
+        public int[] productExceptSelf(int[] nums) {
+
+            if(nums.length <= 1) return nums;
+
+            int [] answer = new int[nums.length];
+            int memo = 1;
+
+            //backward pass
+            for(int i = nums.length -1; i >= 0; i--) {
+                if(i == nums.length - 1) {
+                    answer[i] = 1;
+                }
+                else {
+                    answer[i] = memo * answer[i+1];
+                }
+                memo = nums[i];
+            }
+
+            System.out.println(Arrays.stream(answer).boxed().toList());
+            System.out.println(memo);
+            //forward pass
+            memo = nums[0];
+            nums[0] = 1;
+            for(int i = 0; i < nums.length; i++) {
+                if(i > 0) {
+                    nums[0] = memo * nums[i - 1];
+                    memo = nums[i];
+                    nums[i] = nums[0];
+                }
+            }
+            System.out.println(Arrays.stream(nums).boxed().toList());
+            for(int i = 1; i < nums.length; i++) {
+                answer[i] = nums[i] * answer[i];
+            }
+
+            return answer;
+        }
+    }
+}
