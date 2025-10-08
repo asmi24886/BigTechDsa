e12e74af58099515a790ed456106c1224beb5457
asmi2 <asmi24886@gmail.com>
2025-10-08 23:43:46 +0530

    ok

--- src/main/java/com/solutions/neetcode/linkedlist/LC19/RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/solutions/neetcode/linkedlist/LC19/RemoveNthNodeFromEndOfList.java
@@ -4,7 +4,31 @@ public class RemoveNthNodeFromEndOfList {
 
     class Solution {
         public ListNode removeNthFromEnd(ListNode head, int n) {
+            return twoPointer(head, n);
+        }
 
+        public ListNode twoPointer(ListNode head, int n) {
+            ListNode dummy = new ListNode(-1, head);
+            ListNode lo = dummy;
+            ListNode hi = head;
+            while(hi != null) {
+
+                if(n > 0) {
+                    n--;
+                    hi = hi.next;
+                    continue;
+                }
+
+                lo = lo.next;
+                hi = hi.next;
+            }
+
+            lo.next = lo.next.next;
+
+            return dummy.next;
+        }
+
+        public ListNode twoPass(ListNode head, int n) {
             int length = 0;
             ListNode node = head;
 
@@ -37,9 +61,9 @@ public class RemoveNthNodeFromEndOfList {
         class ListNode {
             int val;
             ListNode next;
-            ListNode(int x) {
+            ListNode(int x, ListNode p) {
                 val = x;
-                next = null;
+                next = p;
             }
         }
     }
