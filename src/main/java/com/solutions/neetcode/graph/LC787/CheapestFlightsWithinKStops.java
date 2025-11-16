01ca5795ee37cda05a3741130feeaf89c7c51646
asmi2 <asmi24886@gmail.com>
2025-11-16 18:41:29 +0530

    ok

--- src/main/java/com/solutions/neetcode/graph/LC787/CheapestFlightsWithinKStops.java
+++ src/main/java/com/solutions/neetcode/graph/LC787/CheapestFlightsWithinKStops.java
@@ -4,7 +4,90 @@ import java.util.*;
 
 public class CheapestFlightsWithinKStops {
 
+    // BFS with pruning
     class Solution {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            List<List<int[]>> adj = new ArrayList<>();
+            int [] costs = new int[n];
+
+            Arrays.fill(costs, Integer.MAX_VALUE);
+
+            for(int i = 0; i < n; i++) {
+                adj.add(new ArrayList<>());
+            }
+
+            for(int[] f : flights) {
+                adj.get(f[0]).add(new int[]{f[1], f[2]});
+            }
+
+            Queue<int []> queue = new LinkedList<>();
+            queue.add(new int []{src, 0, 0});
+
+            while(!queue.isEmpty()) {
+                int [] item = queue.poll();
+                int node = item[0], steps = item[1], cost = item[2];
+
+                if(steps > k) continue;
+
+                List<int []> list = adj.get(node);
+
+                for(int [] nei : list) {
+
+                    int updatedCost = cost + nei[1];
+                    if(updatedCost < costs[nei[0]]) {
+                        costs[nei[0]] = updatedCost;
+                        queue.add(new int[] {nei[0], steps+1, updatedCost});
+                    }
+                }
+            }
+
+            return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
+        }
+    }
+    // modified bellman ford
+    /*
+    [[1,0,5],[2,1,5],[3,0,2],[1,3,2],[4,1,1],[2,4,1]]
+
+    loop 1:
+    og = [i, i, 0, i, i]
+    tmp = [i, 5, 0, i, 1 ]
+
+    loop 2:
+    og = [i, 5, 0, i, 1 ]
+    tmp = [10, 2, 0, 7, 1]
+
+    loop 3:
+    og = [10, 2, 0, 7, 1]
+    tmp = [7, 2, 0, 7, 1] 9 gets updated instead of 7 for 3,0,2 if are checking old values and thats wrong
+     */
+    class Solution2 {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+            int [] costs  = new int[n];
+            Arrays.fill(costs, Integer.MAX_VALUE);
+            costs[src] = 0;
+
+            for(int i = 0; i <= k; i++) {
+                int [] temp = costs.clone();
+                for(int [] f : flights) {
+                    int source = f[0], destination = f[1], cost = f[2];
+                    if(costs[source] == Integer.MAX_VALUE) continue;
+
+                    int updatedCost = costs[source] + cost; // ensures you are not taking a multi chain edge update, else it defeats k+1 edges purpose with 1 edge at a time
+                    if( updatedCost < temp[destination] ) {
+                        temp[destination] = updatedCost;
+                    }
+                }
+
+                costs = temp;
+            }
+
+            return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
+        }
+    }
+
+    //Djikstras + pruning
+    class Solution3 {
         public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
             List<List<int[]>> adj = new ArrayList<>();
             for(int i = 0; i < n; i++) {
@@ -42,10 +125,10 @@ public class CheapestFlightsWithinKStops {
                     int newCost = cost + price;
                     int updatedStops = stops + 1;
 
-                    if(newCost < minCost[nextNode][updatedStops]) {
-                        minCost[nextNode][updatedStops] = newCost;
-                        pq.offer(new int[]{nextNode, newCost, updatedStops});
-                    }
+                    if(newCost >= minCost[nextNode][updatedStops]) continue;
+
+                    minCost[nextNode][updatedStops] = newCost;
+                    pq.offer(new int[]{nextNode, newCost, updatedStops});
                 }
             }
 
