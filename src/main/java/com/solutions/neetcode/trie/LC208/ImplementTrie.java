fcaba8fd4ba7e44b92d178223fe26f62b4aff367
asmi2 <asmi24886@gmail.com>
2025-11-19 22:16:56 +0530

    ok

---
+++ src/main/java/com/solutions/neetcode/trie/LC208/ImplementTrie.java
@@ -0,0 +1,59 @@
+package com.solutions.neetcode.trie.LC208;
+
+import java.util.HashMap;
+
+public class ImplementTrie {
+
+    class TrieNode {
+        HashMap<Character, TrieNode> nodes = new HashMap<>();
+        boolean endOfWord = false;
+    }
+    class Trie {
+        TrieNode root;
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+
+            char [] chars = word.toCharArray();
+            TrieNode trieNode = root;
+            for(char c : chars) {
+                trieNode.nodes.putIfAbsent(c, new TrieNode());
+                trieNode = trieNode.nodes.get(c);
+            }
+
+            trieNode.endOfWord = true;
+        }
+
+        public boolean search(String word) {
+            char [] chars = word.toCharArray();
+            TrieNode trieNode = root;
+            for(char c : chars) {
+                if(!trieNode.nodes.containsKey(c)) return false;
+                trieNode = trieNode.nodes.get(c);
+            }
+
+            return trieNode.endOfWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            char [] chars = prefix.toCharArray();
+            TrieNode trieNode = root;
+            for(char c : chars) {
+                if(!trieNode.nodes.containsKey(c)) return false;
+                trieNode = trieNode.nodes.get(c);
+            }
+
+            return true;
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}
