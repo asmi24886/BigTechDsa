c7de7087758ea5b1096211ff086a2807dd3c645f
asmi2 <asmi24886@gmail.com>
2026-09-03 22:01:07 +0530

    ok

---
+++ src/main/java/com/solutions/neetcode/binarysearch/LC35/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+package com.solutions.neetcode.binarysearch.LC35;
+
+public class SearchInsertPosition {
+
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+
+            int l = 0;
+            int r = nums.length-1;
+
+            while(l <= r) {
+
+                int mid  = l + (r-l)/2;
+
+                if(nums[mid] == target)
+                    return mid;
+
+                if(l == r) {
+                    if(target > nums[l]) return l+1;
+                    else return l;
+                }
+
+                if(target > nums[mid]) {
+                    l=mid+1;
+                }
+                else {
+                    r = mid-1;
+                }
+            }
+
+            return l;
+        }
+    }
+}
